55339cd2c07e28c4cff90abb0111d6e1ac87af8d
Lorelai-anne <lorelai.anne03@gmail.com>
2026-02-03 23:24:40 -0500

    Update FadeIn.cs

--- Mixed Reality Ritual/Assets/Scripts/FadeIn.cs
+++ Mixed Reality Ritual/Assets/Scripts/FadeIn.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -31,19 +32,22 @@ public class FadeIn : MonoBehaviour
 
             foreach (Renderer r in renderers)
             {
-                // Put material into fade mode
-                r.material.SetFloat("_Mode", 2);
-                r.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                r.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                r.material.SetInt("_ZWrite", 0);
-                r.material.DisableKeyword("_ALPHATEST_ON");
-                r.material.EnableKeyword("_BLEND_ON");
-                r.material.DisableKeyword("_PREMULTIPLY_ON");
-                r.material.renderQueue = 3000;
+                foreach (Material m in r.materials)
+                {
+                    // Switch to Fade mode
+                    m.SetFloat("_Mode", 2);
+                    m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    m.SetInt("_ZWrite", 0);
+                    m.DisableKeyword("_ALPHATEST_ON");
+                    m.EnableKeyword("_BLEND_ON");
+                    m.DisableKeyword("_PREMULTIPLY_ON");
+                    m.renderQueue = 3000;
 
-                // Set alpha to 0
-                Color c = r.material.color;
-                r.material.color = new Color(c.r, c.g, c.b, 0f);
+                    // Make fully transparent
+                    Color c = m.color;
+                    m.color = new Color(c.r, c.g, c.b, 0f);
+                }
             }
 
             foreach (MaskableGraphic g in graphics)
@@ -53,6 +57,7 @@ public class FadeIn : MonoBehaviour
             }
         }
     }
+
 
 
     public IEnumerator FadeRoutine(GameObject[] objects, bool fadeIn, float duration)
@@ -66,10 +71,21 @@ public class FadeIn : MonoBehaviour
             Renderer[] renderers = objects[i].GetComponentsInChildren<Renderer>(true);
             MaskableGraphic[] graphics = objects[i].GetComponentsInChildren<MaskableGraphic>(true);
 
-            // Cache starting colors for ALL children
-            Color[] rendererBase = new Color[renderers.Length];
-            for (int r = 0; r < renderers.Length; r++)
-                rendererBase[r] = renderers[r].material.color;
+            // Cache base colors
+            List<Material[]> allMaterials = new List<Material[]>();
+            List<Color[]> baseColors = new List<Color[]>();
+
+            foreach (Renderer r in renderers)
+            {
+                Material[] mats = r.materials;
+                allMaterials.Add(mats);
+
+                Color[] cols = new Color[mats.Length];
+                for (int m = 0; m < mats.Length; m++)
+                    cols[m] = mats[m].color;
+
+                baseColors.Add(cols);
+            }
 
             Color[] graphicBase = new Color[graphics.Length];
             for (int g = 0; g < graphics.Length; g++)
@@ -80,10 +96,13 @@ public class FadeIn : MonoBehaviour
                 counter += Time.deltaTime;
                 float alpha = Mathf.Lerp(startAlpha, endAlpha, counter / duration);
 
-                for (int r = 0; r < renderers.Length; r++)
+                for (int r = 0; r < allMaterials.Count; r++)
                 {
-                    Color c = rendererBase[r];
-                    renderers[r].material.color = new Color(c.r, c.g, c.b, alpha);
+                    for (int m = 0; m < allMaterials[r].Length; m++)
+                    {
+                        Color c = baseColors[r][m];
+                        allMaterials[r][m].color = new Color(c.r, c.g, c.b, alpha);
+                    }
                 }
 
                 for (int g = 0; g < graphics.Length; g++)
